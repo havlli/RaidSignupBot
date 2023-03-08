0fc780911cebe830aea08df0843daeaa24dff6d3
Havlli <havlliczech@gmail.com>
2023-03-09 00:01:05 +0100

    refactored values for SelectMenu

--- src/main/java/com/github/havlli/raidsignupbot/events/createevent/RaidSelectMenu.java
+++ src/main/java/com/github/havlli/raidsignupbot/events/createevent/RaidSelectMenu.java
@@ -9,13 +9,15 @@ import java.util.List;
 public class RaidSelectMenu {
     public static ActionRow getRaidSelectMenu() {
         List<SelectMenu.Option> eventOptions = new ArrayList<>();
-        eventOptions.add(SelectMenu.Option.of("Molten Core - Normal", "mc-n"));
-        eventOptions.add(SelectMenu.Option.of("Molten Core - Heroic", "mc-h"));
-        eventOptions.add(SelectMenu.Option.of("Molten Core - Mythic", "mc-m"));
-        eventOptions.add(SelectMenu.Option.of("Molten Core - Ascended", "mc-a"));
-        eventOptions.add(SelectMenu.Option.of("Onyxia - Normal", "ony-n"));
-        SelectMenu selectMenu = SelectMenu.of("raid-select", eventOptions
-        ).withMaxValues(3).withMinValues(1);
+        eventOptions.add(SelectMenu.Option.of("Molten Core - Normal", "Molten Core - Normal"));
+        eventOptions.add(SelectMenu.Option.of("Molten Core - Heroic", "Molten Core - Heroic"));
+        eventOptions.add(SelectMenu.Option.of("Molten Core - Mythic", "Molten Core - Mythic"));
+        eventOptions.add(SelectMenu.Option.of("Molten Core - Ascended", "Molten Core - Ascended"));
+        eventOptions.add(SelectMenu.Option.of("Onyxia - Normal", "Onyxia - Normal"));
+        SelectMenu selectMenu = SelectMenu.of("raid-select", eventOptions)
+                .withMaxValues(3).withMinValues(1);
         return ActionRow.of(selectMenu);
+
+
     }
 }
