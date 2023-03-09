45c68fcbbb290182fa58c2198fb187333423382d
Havlli <havlliczech@gmail.com>
2023-03-09 17:41:20 +0100

    testing textInput component

--- src/main/java/com/github/havlli/raidsignupbot/events/test/TestEvent.java
+++ src/main/java/com/github/havlli/raidsignupbot/events/test/TestEvent.java
@@ -3,7 +3,9 @@ package com.github.havlli.raidsignupbot.events.test;
 import com.github.havlli.raidsignupbot.events.EventHandler;
 import discord4j.core.event.domain.Event;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.component.TextInput;
 import discord4j.core.object.entity.Message;
+import discord4j.core.spec.MessageCreateSpec;
 import reactor.core.publisher.Mono;
 
 public class TestEvent implements EventHandler {
@@ -15,9 +17,15 @@ public class TestEvent implements EventHandler {
     @Override
     public Mono<Message> handleEvent(Event event) {
         MessageCreateEvent messageCreateEvent = (MessageCreateEvent) event;
+
+        TextInput textInput = TextInput.small("textinput","Label");
+
         if (messageCreateEvent.getMessage().getContent().equals("test")) {
             return messageCreateEvent.getMessage().getChannel()
-                    .flatMap(channel -> channel.createMessage("Tested!"));
+                    .flatMap(channel -> channel.createMessage(MessageCreateSpec
+                            .builder()
+                                    .content("Testing")
+                            .build()));
         }
 
         return Mono.empty();
