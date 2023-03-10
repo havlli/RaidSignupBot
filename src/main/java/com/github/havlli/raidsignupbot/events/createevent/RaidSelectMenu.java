d464dd996a2cb6b70598f6568a0614092c3ce6db
Havlli <havlliczech@gmail.com>
2023-03-10 18:18:37 +0100

    added new SelectMenu components - getRaidSizeSelect and getTextChannelSelect methods

--- src/main/java/com/github/havlli/raidsignupbot/events/createevent/RaidSelectMenu.java
+++ src/main/java/com/github/havlli/raidsignupbot/events/createevent/RaidSelectMenu.java
@@ -2,6 +2,7 @@ package com.github.havlli.raidsignupbot.events.createevent;
 
 import discord4j.core.object.component.ActionRow;
 import discord4j.core.object.component.SelectMenu;
+import discord4j.core.object.entity.channel.TextChannel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ public class RaidSelectMenu {
         eventOptions.add(SelectMenu.Option.of("Molten Core - Ascended", "Molten Core - Ascended"));
         eventOptions.add(SelectMenu.Option.of("Onyxia - Normal", "Onyxia - Normal"));
         SelectMenu selectMenu = SelectMenu.of("raid-select", eventOptions)
+                .withPlaceholder("Choose Raids for this event!")
                 .withMaxValues(3).withMinValues(1);
         return ActionRow.of(selectMenu);
     }
@@ -26,6 +28,18 @@ public class RaidSelectMenu {
         selectOptions.add(SelectMenu.Option.of("20", "20"));
         selectOptions.add(SelectMenu.Option.of("25", "25"));
         SelectMenu selectMenu = SelectMenu.of("raid-size", selectOptions)
+                .withPlaceholder("Choose maximum raid size!")
+                .withMaxValues(1).withMinValues(1);
+        return ActionRow.of(selectMenu);
+    }
+
+    public static ActionRow getTextChannelSelect(List<TextChannel> textChannels) {
+        List<SelectMenu.Option> selectOptions = new ArrayList<>();
+        for (TextChannel textChannel : textChannels) {
+            selectOptions.add(SelectMenu.Option.of(textChannel.getName(), textChannel.getId().toString()));
+        }
+        SelectMenu selectMenu = SelectMenu.of("raid-size", selectOptions)
+                .withPlaceholder("Choose channel to post signup in!")
                 .withMaxValues(1).withMinValues(1);
         return ActionRow.of(selectMenu);
     }
