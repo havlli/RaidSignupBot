340a98d3380c65d5bc4b9bf7edd37c4167bef0fc
Havlli <havlliczech@gmail.com>
2023-03-14 15:09:07 +0100

    added multiple ActionRow -added getReserveRow -added getConfirmationRow

--- src/main/java/com/github/havlli/raidsignupbot/component/ActionRows.java
+++ src/main/java/com/github/havlli/raidsignupbot/component/ActionRows.java
@@ -1,13 +1,14 @@
-package com.github.havlli.raidsignupbot.events.createevent;
+package com.github.havlli.raidsignupbot.component;
 
 import discord4j.core.object.component.ActionRow;
+import discord4j.core.object.component.Button;
 import discord4j.core.object.component.SelectMenu;
 import discord4j.core.object.entity.channel.TextChannel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RaidSelectMenu {
+public class ActionRows {
     public static ActionRow getRaidSelectMenu() {
         List<SelectMenu.Option> eventOptions = new ArrayList<>();
         eventOptions.add(SelectMenu.Option.of("Molten Core - Normal", "Molten Core - Normal"));
@@ -36,11 +37,23 @@ public class RaidSelectMenu {
     public static ActionRow getTextChannelSelect(List<TextChannel> textChannels) {
         List<SelectMenu.Option> selectOptions = new ArrayList<>();
         for (TextChannel textChannel : textChannels) {
-            selectOptions.add(SelectMenu.Option.of(textChannel.getName(), textChannel.getId().toString()));
+            selectOptions.add(SelectMenu.Option.of(textChannel.getName(), textChannel.getId().asString()));
         }
         SelectMenu selectMenu = SelectMenu.of("destination-channel", selectOptions)
                 .withPlaceholder("Choose channel to post signup in!")
                 .withMaxValues(1).withMinValues(1);
         return ActionRow.of(selectMenu);
+    }
+
+    public static ActionRow getReserveRow() {
+        Button buttonYes = Button.primary("reserveYes","Yes");
+        Button buttonNo = Button.danger("reserveNo", "No");
+        return ActionRow.of(buttonYes, buttonNo);
+    }
+
+    public static ActionRow getConfirmationRow() {
+        Button confirm = Button.primary("confirm","Confirm");
+        Button cancel = Button.danger("cancel", "Cancel");
+        return ActionRow.of(confirm, cancel);
     }
 }
