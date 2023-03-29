6754e96a8ce1f312aa5dea27b4725e56675e97b0
Havlli <havlliczech@gmail.com>
2023-03-29 23:18:49 +0200

    removed printOnLogon events subscribing to ReadyEvent were moved to seperated class OnReadyEvent due to preparation for implementing Schedulers

--- src/main/java/com/github/havlli/raidsignupbot/client/InitiateClient.java
+++ src/main/java/com/github/havlli/raidsignupbot/client/InitiateClient.java
@@ -2,9 +2,6 @@ package com.github.havlli.raidsignupbot.client;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
-import discord4j.core.event.domain.lifecycle.ReadyEvent;
-import discord4j.core.object.entity.User;
-import reactor.core.publisher.Mono;
 
 public class InitiateClient {
 
@@ -34,19 +31,11 @@ public class InitiateClient {
         if (gateway == null) gateway = getClient()
                                             .login()
                                             .block();
-        printOnLogin();
 
         return gateway;
     }
 
     public static void onDisconnect() {
         getGateway().onDisconnect().block();
-    }
-
-    private static void printOnLogin() {
-        gateway.on(ReadyEvent.class, event -> Mono.fromRunnable(() -> {
-            final User self = event.getSelf();
-            System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
-        })).subscribe();
     }
 }
