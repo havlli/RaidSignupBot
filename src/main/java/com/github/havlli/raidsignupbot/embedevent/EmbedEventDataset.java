328eba8733ac735e43f6672aba43c87c7623fc13
Havlli <havlliczech@gmail.com>
2023-03-31 21:17:24 +0200

    added EmbedEventDataset.java singleton class that loads all active EmbedEvent objects from database and keep them in HashSet

---
+++ src/main/java/com/github/havlli/raidsignupbot/embedevent/EmbedEventDataset.java
@@ -0,0 +1,26 @@
+package com.github.havlli.raidsignupbot.embedevent;
+
+import java.util.HashSet;
+
+public class EmbedEventDataset {
+    private static EmbedEventDataset singleton = null;
+    private EmbedEventDataset() {
+        embedEventHashSet = new HashSet<>();
+        populateEmbedEventHashSet();
+    }
+
+    public static EmbedEventDataset getInstance() {
+        if(singleton == null) singleton = new EmbedEventDataset();
+        return singleton;
+    }
+
+    private static HashSet<EmbedEvent> embedEventHashSet;
+
+    private void populateEmbedEventHashSet() {
+        embedEventHashSet = EmbedEventDAO.fetchActiveEmbedEvents();
+    }
+
+    public HashSet<EmbedEvent> getData() {
+        return embedEventHashSet;
+    }
+}
