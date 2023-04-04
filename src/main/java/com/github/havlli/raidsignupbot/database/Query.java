7e59a709d4a49154010a40240deac02bdb367b5e
Havlli <havlliczech@gmail.com>
2023-04-04 21:50:41 +0200

    refactoring

--- src/main/java/com/github/havlli/raidsignupbot/database/Query.java
+++ src/main/java/com/github/havlli/raidsignupbot/database/Query.java
@@ -1,24 +1,45 @@
 package com.github.havlli.raidsignupbot.database;
 
 import com.github.havlli.raidsignupbot.database.structure.EmbedEventColumn;
+import com.github.havlli.raidsignupbot.database.structure.SignupUserColumn;
 import com.github.havlli.raidsignupbot.database.structure.Table;
 
 public class Query {
     public static final String INSERT_EMBED_EVENT =
             "INSERT INTO "+ Table.EMBED_EVENT +
-            " (" + EmbedEventColumn.ID + "," +
-            EmbedEventColumn.NAME + "," +
-            EmbedEventColumn.DESCRIPTION + "," +
-            EmbedEventColumn.DATE_TIME + "," +
-            EmbedEventColumn.INSTANCES + "," +
-            EmbedEventColumn.MEMBER_SIZE + "," +
-            EmbedEventColumn.RESERVE_ENABLED + "," +
-            EmbedEventColumn.DESTINATION_CHANNEL + "," +
-            EmbedEventColumn.AUTHOR + "," +
-            EmbedEventColumn.ACTIVE + ")" +
-            " VALUES (?,?,?,?,?,?,?,?,?,?)";
+             " (" + EmbedEventColumn.ID + "," +
+                    EmbedEventColumn.NAME + "," +
+                    EmbedEventColumn.DESCRIPTION + "," +
+                    EmbedEventColumn.DATE_TIME + "," +
+                    EmbedEventColumn.INSTANCES + "," +
+                    EmbedEventColumn.MEMBER_SIZE + "," +
+                    EmbedEventColumn.RESERVE_ENABLED + "," +
+                    EmbedEventColumn.DESTINATION_CHANNEL + "," +
+                    EmbedEventColumn.AUTHOR + "," +
+                    EmbedEventColumn.ACTIVE + ") " +
+            "VALUES (?,?,?,?,?,?,?,?,?,?)";
 
     public static final String SELECT_ACTIVE_EMBED_EVENTS =
             "SELECT * FROM " + Table.EMBED_EVENT +
-            " WHERE " + EmbedEventColumn.ACTIVE + " = 1";
+                    " WHERE " + EmbedEventColumn.ACTIVE + " = 1";
+
+    public static final String INSERT_SIGNUP_USER =
+            "INSERT INTO " + Table.SIGNUP_USER +
+             " (" + SignupUserColumn.USER_ID + "," +
+                    SignupUserColumn.USERNAME + "," +
+                    SignupUserColumn.FIELD_INDEX + "," +
+                    SignupUserColumn.ORDER + "," +
+                    SignupUserColumn.EMBED_EVENT_ID + ") "
+            + "VALUES (?,?,?,?,?)";
+
+    public static final String SELECT_SIGNUP_USER_BY_ID =
+            "SELECT * FROM " + Table.SIGNUP_USER +
+                    " WHERE " + SignupUserColumn.EMBED_EVENT_ID + " LIKE ?" +
+                    " ORDER BY " + SignupUserColumn.ORDER;
+
+    public static final String UPDATE_SIGNUP_USER_FIELD_INDEX =
+            "UPDATE " + Table.SIGNUP_USER +
+                    " SET " + SignupUserColumn.FIELD_INDEX + " = ?" +
+                    " WHERE " + SignupUserColumn.USER_ID + " = ?" +
+                    " AND " + SignupUserColumn.EMBED_EVENT_ID + " = ?";
 }
--- src/main/java/com/github/havlli/raidsignupbot/database/structure/SignupUserColumn.java
+++ src/main/java/com/github/havlli/raidsignupbot/database/structure/SignupUserColumn.java
@@ -4,7 +4,7 @@ public enum SignupUserColumn {
     USER_ID("user_id"),
     USERNAME("username"),
     FIELD_INDEX("field_index"),
-    ORDER("order"),
+    ORDER("ordering"),
     EMBED_EVENT_ID("embed_event_id");
 
     private final String columnName;
