057671326f477c1b9fe81c9a4afed65791497463
Havlli <havlliczech@gmail.com>
2023-04-05 18:09:26 +0200

    added new method for populating signupUserList in each EmbedEvent object

--- src/main/java/com/github/havlli/raidsignupbot/embedevent/EmbedEventDataset.java
+++ src/main/java/com/github/havlli/raidsignupbot/embedevent/EmbedEventDataset.java
@@ -1,12 +1,17 @@
 package com.github.havlli.raidsignupbot.embedevent;
 
+import com.github.havlli.raidsignupbot.events.createevent.SignupUser;
+import com.github.havlli.raidsignupbot.events.createevent.SignupUserDAO;
+
 import java.util.HashSet;
+import java.util.List;
 
 public class EmbedEventDataset {
     private static EmbedEventDataset singleton = null;
     private EmbedEventDataset() {
         embedEventHashSet = new HashSet<>();
         populateEmbedEventHashSet();
+        forEachEmbedEventPopulateSignupUserList();
     }
 
     public static EmbedEventDataset getInstance() {
@@ -18,6 +23,14 @@ public class EmbedEventDataset {
 
     private void populateEmbedEventHashSet() {
         embedEventHashSet = EmbedEventDAO.fetchActiveEmbedEvents();
+    }
+
+    private void forEachEmbedEventPopulateSignupUserList() {
+        embedEventHashSet.forEach(embedEvent -> {
+            String embedEventId = embedEvent.getEmbedId().toString();
+            List<SignupUser> signupUserList = SignupUserDAO.selectSignupUsersById(embedEventId);
+            embedEvent.setSignupUsers(signupUserList);
+        });
     }
 
     public HashSet<EmbedEvent> getData() {
