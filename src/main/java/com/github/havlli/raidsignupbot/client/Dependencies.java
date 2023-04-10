166d05916f8c27c5ecf5b934666683665af37a83
Havlli <havlliczech@gmail.com>
2023-04-10 18:20:07 +0200

    added Logger to dependencies

--- src/main/java/com/github/havlli/raidsignupbot/client/Dependencies.java
+++ src/main/java/com/github/havlli/raidsignupbot/client/Dependencies.java
@@ -1,20 +1,29 @@
 package com.github.havlli.raidsignupbot.client;
 
+import com.github.havlli.raidsignupbot.database.ConnectionProvider;
 import com.github.havlli.raidsignupbot.database.JdbcConnectionProvider;
 import com.github.havlli.raidsignupbot.embedevent.EmbedEventDAO;
+import com.github.havlli.raidsignupbot.logger.ConsoleLogger;
+import com.github.havlli.raidsignupbot.logger.Logger;
+import com.github.havlli.raidsignupbot.logger.MessagePrinter;
+import com.github.havlli.raidsignupbot.logger.TextFormatter;
 import com.github.havlli.raidsignupbot.signupuser.SignupUserDAO;
 
 public class Dependencies {
 
     private static Dependencies instance;
-    private final JdbcConnectionProvider jdbcProvider;
+    private final ConnectionProvider connectionProvider;
     private final SignupUserDAO signupUserDAO;
     private final EmbedEventDAO embedEventDAO;
+    private final Logger logger;
+
 
     private Dependencies() {
-        this.jdbcProvider = new JdbcConnectionProvider();
-        this.signupUserDAO = new SignupUserDAO(jdbcProvider);
-        this.embedEventDAO = new EmbedEventDAO(jdbcProvider);
+        this.connectionProvider = new JdbcConnectionProvider();
+        this.signupUserDAO = new SignupUserDAO(connectionProvider);
+        this.embedEventDAO = new EmbedEventDAO(connectionProvider);
+        this.logger = new ConsoleLogger(new MessagePrinter(), new TextFormatter());
+
     }
 
     public static Dependencies getInstance() {
@@ -29,5 +38,9 @@ public class Dependencies {
 
     public EmbedEventDAO getEmbedEventDAO() {
         return embedEventDAO;
+    }
+
+    public Logger getLogger() {
+        return logger;
     }
 }
