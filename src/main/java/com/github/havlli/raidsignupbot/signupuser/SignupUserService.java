c0ae541bb2b97d25ebb37d1c5e64644ec040f5e4
Havlli <havlliczech@gmail.com>
2023-04-11 19:09:32 +0200

    removed dependency -removed dependency for list of signupUsers from constructor

--- src/main/java/com/github/havlli/raidsignupbot/signupuser/SignupUserService.java
+++ src/main/java/com/github/havlli/raidsignupbot/signupuser/SignupUserService.java
@@ -1,34 +1,28 @@
 package com.github.havlli.raidsignupbot.signupuser;
 
-import java.util.Collections;
 import java.util.List;
 
 public class SignupUserService {
-    private final List<SignupUser> signupUsers;
     private final SignupUserDAO signupUserDAO;
 
-    public SignupUserService(List<SignupUser> signupUsers, SignupUserDAO signupUserDAO) {
-        this.signupUsers = signupUsers;
+    public SignupUserService(SignupUserDAO signupUserDAO) {
         this.signupUserDAO = signupUserDAO;
     }
 
-    public List<SignupUser> getSignupUsers() {
-        return Collections.unmodifiableList(signupUsers);
-    }
-
-    public SignupUser getSignupUser(String userId) {
+    public SignupUser getSignupUser(String userId, List<SignupUser> signupUsers) {
         return signupUsers.stream()
                 .filter(signupUser -> signupUser.getId().equals(userId))
                 .findFirst()
                 .orElse(null);
     }
 
-    public void addSignupUser(SignupUser signupUser, String embedEventId) {
+    public void addSignupUser(SignupUser signupUser, List<SignupUser> signupUsers, String embedEventId) {
         signupUsers.add(signupUser);
         signupUserDAO.insertSignupUser(signupUser, embedEventId);
     }
 
-    public void updateSignupUser(SignupUser signupUser, String embedEventId) {
+    public void updateSignupUser(SignupUser signupUser, String embedEventId, int fieldIndex) {
+        signupUser.setFieldIndex(fieldIndex);
         signupUserDAO.updateSignupUserFieldIndex(signupUser.getId(), signupUser.getFieldIndex(), embedEventId);
     }
 }
