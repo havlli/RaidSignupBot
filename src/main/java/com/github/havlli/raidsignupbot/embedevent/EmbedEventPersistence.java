032c4a48f837fa57edfd2c3fd0e8f3e92ea1a43d
Havlli <havlliczech@gmail.com>
2023-04-11 19:07:40 +0200

    moved singleton initialization to Dependencies.java

--- src/main/java/com/github/havlli/raidsignupbot/embedevent/EmbedEventPersistence.java
+++ src/main/java/com/github/havlli/raidsignupbot/embedevent/EmbedEventPersistence.java
@@ -1,6 +1,5 @@
 package com.github.havlli.raidsignupbot.embedevent;
 
-import com.github.havlli.raidsignupbot.client.Dependencies;
 import com.github.havlli.raidsignupbot.signupuser.SignupUser;
 import com.github.havlli.raidsignupbot.signupuser.SignupUserDAO;
 
@@ -8,26 +7,15 @@ import java.util.HashSet;
 import java.util.List;
 
 public class EmbedEventPersistence {
-    private static EmbedEventPersistence singleton = null;
     private static HashSet<EmbedEvent> embedEventHashSet;
     private final EmbedEventDAO embedEventDAO;
     private final SignupUserDAO signupUserDAO;
-    private EmbedEventPersistence(EmbedEventDAO embedEventDAO, SignupUserDAO signupUserDAO) {
+    public EmbedEventPersistence(EmbedEventDAO embedEventDAO, SignupUserDAO signupUserDAO) {
         this.embedEventDAO = embedEventDAO;
         this.signupUserDAO = signupUserDAO;
         embedEventHashSet = new HashSet<>();
         populateEmbedEventHashSet();
         forEachEmbedEventPopulateSignupUserList();
-    }
-
-    public static EmbedEventPersistence getInstance() {
-        if(singleton == null) {
-            singleton = new EmbedEventPersistence(
-                    Dependencies.getInstance().getEmbedEventDAO(),
-                    Dependencies.getInstance().getSignupUserDAO()
-            );
-        }
-        return singleton;
     }
 
     private void populateEmbedEventHashSet() {
