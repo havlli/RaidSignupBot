7477da20e907c1fa3cd46051aa9ab9ef59d5bb88
Havlli <havlliczech@gmail.com>
2023-04-11 19:05:45 +0200

    added new singleton classes

--- src/main/java/com/github/havlli/raidsignupbot/client/Dependencies.java
+++ src/main/java/com/github/havlli/raidsignupbot/client/Dependencies.java
@@ -3,24 +3,33 @@ package com.github.havlli.raidsignupbot.client;
 import com.github.havlli.raidsignupbot.database.ConnectionProvider;
 import com.github.havlli.raidsignupbot.database.JdbcConnectionProvider;
 import com.github.havlli.raidsignupbot.embedevent.EmbedEventDAO;
+import com.github.havlli.raidsignupbot.embedevent.EmbedEventPersistence;
+import com.github.havlli.raidsignupbot.embedevent.EmbedEventService;
 import com.github.havlli.raidsignupbot.logger.ConsoleLogger;
 import com.github.havlli.raidsignupbot.logger.Logger;
 import com.github.havlli.raidsignupbot.logger.MessagePrinter;
 import com.github.havlli.raidsignupbot.logger.TextFormatter;
 import com.github.havlli.raidsignupbot.signupuser.SignupUserDAO;
+import com.github.havlli.raidsignupbot.signupuser.SignupUserService;
 
 public class Dependencies {
 
     private static Dependencies instance;
     private final SignupUserDAO signupUserDAO;
+    private final SignupUserService signupUserService;
     private final EmbedEventDAO embedEventDAO;
+    private final EmbedEventPersistence embedEventPersistence;
+    private final EmbedEventService embedEventService;
     private final Logger logger;
 
 
     private Dependencies() {
         ConnectionProvider connectionProvider = new JdbcConnectionProvider();
         this.signupUserDAO = new SignupUserDAO(connectionProvider);
+        this.signupUserService = new SignupUserService(signupUserDAO);
         this.embedEventDAO = new EmbedEventDAO(connectionProvider);
+        this.embedEventPersistence = new EmbedEventPersistence(embedEventDAO, signupUserDAO);
+        this.embedEventService = new EmbedEventService(embedEventDAO, embedEventPersistence);
         this.logger = new ConsoleLogger(new MessagePrinter(), new TextFormatter());
 
     }
@@ -35,8 +44,20 @@ public class Dependencies {
         return signupUserDAO;
     }
 
+    public SignupUserService getSignupUserService() {
+        return signupUserService;
+    }
+
     public EmbedEventDAO getEmbedEventDAO() {
         return embedEventDAO;
+    }
+
+    public EmbedEventPersistence getEmbedEventPersistence() {
+        return embedEventPersistence;
+    }
+
+    public EmbedEventService getEmbedEventService() {
+        return embedEventService;
     }
 
     public Logger getLogger() {
