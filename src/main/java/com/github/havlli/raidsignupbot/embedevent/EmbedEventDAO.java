9caf4fcd4094b2e2b376292dfcff0e043b94daf0
Havlli <havlliczech@gmail.com>
2023-04-14 21:25:46 +0200

    refactored to implement new EmbedEventBuilder

--- src/main/java/com/github/havlli/raidsignupbot/embedevent/EmbedEventDAO.java
+++ src/main/java/com/github/havlli/raidsignupbot/embedevent/EmbedEventDAO.java
@@ -10,9 +10,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Arrays;
 import java.util.HashSet;
 
 public class EmbedEventDAO {
@@ -38,14 +35,14 @@ public class EmbedEventDAO {
 
     private static PreparedStatement createInsertEmbedEventPrep(Connection connection, EmbedEvent embedEvent) throws SQLException {
         PreparedStatement insertEmbedEventPrep = connection.prepareStatement(Query.INSERT_EMBED_EVENT);
-        insertEmbedEventPrep.setString(1, embedEvent.getEmbedId().toString());
+        insertEmbedEventPrep.setString(1, embedEvent.getEmbedId());
         insertEmbedEventPrep.setString(2, embedEvent.getName());
         insertEmbedEventPrep.setString(3, embedEvent.getDescription());
-        insertEmbedEventPrep.setString(4, embedEvent.getDate().toString() + " " + embedEvent.getTime().toString());
-        insertEmbedEventPrep.setString(5, String.join(", ", embedEvent.getInstances()));
+        insertEmbedEventPrep.setString(4, embedEvent.getDateTimeString(" "));
+        insertEmbedEventPrep.setString(5, embedEvent.getInstances());
         insertEmbedEventPrep.setString(6, embedEvent.getMemberSize());
         insertEmbedEventPrep.setInt(7, embedEvent.isReservingEnabled() ? 1 : 0);
-        insertEmbedEventPrep.setString(8, embedEvent.getDestinationChannelId().toString());
+        insertEmbedEventPrep.setString(8, embedEvent.getDestinationChannelId());
         insertEmbedEventPrep.setString(9, embedEvent.getAuthor());
         insertEmbedEventPrep.setInt(10, 1);
         return insertEmbedEventPrep;
@@ -71,21 +68,30 @@ public class EmbedEventDAO {
     }
 
     private static EmbedEvent mapEmbedEventFromResultSet(ResultSet resultSet) throws SQLException {
-        EmbedEvent embedEvent = new EmbedEvent();
-        embedEvent.setEmbedId(resultSet.getLong(EmbedEventColumn.ID.toString()));
-        embedEvent.setName(resultSet.getString(EmbedEventColumn.NAME.toString()));
-        embedEvent.setDescription(resultSet.getString(EmbedEventColumn.DESCRIPTION.toString()));
+        String embedId = resultSet.getString(EmbedEventColumn.ID.toString());
+        String name = resultSet.getString(EmbedEventColumn.NAME.toString());
+        String description = resultSet.getString(EmbedEventColumn.DESCRIPTION.toString());
+        String instances = resultSet.getString(EmbedEventColumn.INSTANCES.toString());
+        String memberSize = resultSet.getString(EmbedEventColumn.MEMBER_SIZE.toString());
+        int reserving = resultSet.getInt(EmbedEventColumn.RESERVE_ENABLED.toString());
+        String destinationChannel = resultSet.getString(EmbedEventColumn.DESTINATION_CHANNEL.toString());
+        String author = resultSet.getString(EmbedEventColumn.AUTHOR.toString());
         String[] datetime = resultSet.getString(EmbedEventColumn.DATE_TIME.toString()).split(" ");
-        embedEvent.setDate(LocalDate.parse(datetime[0]));
-        embedEvent.setTime(LocalTime.parse(datetime[1]));
-        embedEvent.setInstances(Arrays.stream(resultSet.getString(EmbedEventColumn.INSTANCES.toString()).split(", ")).toList());
-        embedEvent.setMemberSize(resultSet.getString(EmbedEventColumn.MEMBER_SIZE.toString()));
-        embedEvent.setReservingEnabled(resultSet.getInt(EmbedEventColumn.RESERVE_ENABLED.toString()) == 1);
-        embedEvent.setDestinationChannelId(resultSet.getLong(EmbedEventColumn.DESTINATION_CHANNEL.toString()));
-        embedEvent.setAuthor(resultSet.getString(EmbedEventColumn.AUTHOR.toString()));
-        embedEvent.setActive(resultSet.getInt(EmbedEventColumn.ACTIVE.toString()) == 1);
+        String date = datetime[0];
+        String time = datetime[1];
 
-        return embedEvent;
+        return EmbedEvent.builder()
+                .addEmbedId(embedId)
+                .addName(name)
+                .addDescription(description)
+                .addInstances(instances)
+                .addMemberSize(memberSize)
+                .addReservingEnabled(reserving)
+                .addDestinationChannel(destinationChannel)
+                .addAuthor(author)
+                .addDate(date)
+                .addTime(time)
+                .build();
     }
 
     public void updateExpiredEmbedEvent(EmbedEvent embedEvent, boolean isActive) {
@@ -102,7 +108,7 @@ public class EmbedEventDAO {
         int active = isActive ? 1 : 0;
         PreparedStatement preparedStatement = connection.prepareStatement(Query.UPDATE_EXPIRED_EMBED_EVENT);
         preparedStatement.setInt(1, active);
-        preparedStatement.setString(2, embedEvent.getEmbedId().toString());
+        preparedStatement.setString(2, embedEvent.getEmbedId());
 
         return preparedStatement;
     }
