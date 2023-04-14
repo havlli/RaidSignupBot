2c5885195a2dc62d996ebb129406561399fb7fe0
Havlli <havlliczech@gmail.com>
2023-04-14 21:27:55 +0200

    refactored to follow changes to implement EmbedEventBuilder

--- src/main/java/com/github/havlli/raidsignupbot/events/createevent/FieldSupplier.java
+++ src/main/java/com/github/havlli/raidsignupbot/events/createevent/FieldSupplier.java
@@ -1,9 +1,10 @@
 package com.github.havlli.raidsignupbot.events.createevent;
 
+import com.github.havlli.raidsignupbot.embedevent.EmbedEvent;
 import discord4j.core.spec.EmbedCreateFields;
 
 import java.util.List;
 
 public interface FieldSupplier {
-    List<EmbedCreateFields.Field> getPopulatedFields();
+    List<EmbedCreateFields.Field> getPopulatedFields(EmbedEvent embedEvent);
 }
--- src/main/java/com/github/havlli/raidsignupbot/events/createevent/InteractionSubscriber.java
+++ src/main/java/com/github/havlli/raidsignupbot/events/createevent/InteractionSubscriber.java
@@ -1,9 +1,10 @@
 package com.github.havlli.raidsignupbot.events.createevent;
 
+import com.github.havlli.raidsignupbot.embedevent.EmbedEvent;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
 import discord4j.core.object.entity.Message;
 import reactor.core.publisher.Mono;
 
 public interface InteractionSubscriber {
-    Mono<Message> handleEvent(ButtonInteractionEvent event);
+    Mono<Message> handleEvent(ButtonInteractionEvent event, EmbedEvent embedEvent);
 }
