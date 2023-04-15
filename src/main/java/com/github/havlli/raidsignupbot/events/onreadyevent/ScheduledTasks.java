d010880c4e165104e4f740e5e48ba0cd1cb2df75
Havlli <havlliczech@gmail.com>
2023-04-15 14:26:40 +0200

    new objects to separate scheduled tasks and task that runs only once at the startup

---
+++ src/main/java/com/github/havlli/raidsignupbot/events/onreadyevent/ScheduledTasks.java
@@ -0,0 +1,74 @@
+package com.github.havlli.raidsignupbot.events.onreadyevent;
+
+import com.github.havlli.raidsignupbot.client.Dependencies;
+import com.github.havlli.raidsignupbot.component.ActionRows;
+import com.github.havlli.raidsignupbot.embedevent.EmbedEventService;
+import com.github.havlli.raidsignupbot.logger.Logger;
+import discord4j.common.util.Snowflake;
+import discord4j.core.event.domain.Event;
+import discord4j.core.spec.MessageEditSpec;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+public class ScheduledTasks {
+
+    private final Logger logger;
+    private final Event event;
+    private final EmbedEventService embedEventService;
+
+    public ScheduledTasks(Event event, Logger logger) {
+        this.logger = logger;
+        this.event = event;
+        this.embedEventService = Dependencies.getInstance()
+                .getEmbedEventService();
+
+    }
+
+    private void run() {
+        printExpiredEmbedEvents();
+        handleExpiredEmbedEvents();
+    }
+
+    public Mono<Void> getSchedulerMono() {
+
+        final int INTERVAL_SECONDS = 5;
+        Duration duration = Duration.ofSeconds(INTERVAL_SECONDS);
+
+        logger.log("Scheduler registered");
+        Scheduler scheduler = Schedulers.newSingle("EmbedScheduler");
+
+        return Mono.fromRunnable(this::run)
+                .delaySubscription(duration)
+                .repeat()
+                .subscribeOn(scheduler)
+                .then();
+    }
+
+    private void printExpiredEmbedEvents() {
+
+        logger.log("Scheduled check for expired EmbedEvents...");
+        embedEventService.getExpiredEmbedEvents()
+                .forEach(embedEvent -> logger.log(embedEvent.getEmbedId()));
+    }
+
+    private void handleExpiredEmbedEvents() {
+
+        embedEventService.getExpiredEmbedEvents()
+                .forEach(embedEvent -> {
+
+                    Snowflake messageId = Snowflake.of(embedEvent.getEmbedId());
+                    Snowflake channelId = Snowflake.of(embedEvent.getDestinationChannelId());
+                    event.getClient()
+                            .getMessageById(channelId, messageId)
+                            .flatMap(message -> message.edit(MessageEditSpec.builder()
+                                    .addComponent(ActionRows.getExpiredRow())
+                                    .build()))
+                            .subscribe();
+
+                    embedEventService.removeEmbedEvent(embedEvent);
+                });
+    }
+}
---
+++ src/main/java/com/github/havlli/raidsignupbot/events/onreadyevent/StartupTasks.java
@@ -0,0 +1,42 @@
+package com.github.havlli.raidsignupbot.events.onreadyevent;
+
+import com.github.havlli.raidsignupbot.client.Dependencies;
+import com.github.havlli.raidsignupbot.embedevent.EmbedEvent;
+import com.github.havlli.raidsignupbot.events.createevent.EmbedBuilder;
+import com.github.havlli.raidsignupbot.logger.Logger;
+import discord4j.core.event.EventDispatcher;
+import discord4j.core.event.domain.Event;
+
+import java.util.HashSet;
+
+public class StartupTasks {
+
+    private final Logger logger;
+    private final Event event;
+
+    public StartupTasks(Event event, Logger logger) {
+        this.event = event;
+        this.logger = logger;
+    }
+
+    public void run() {
+        subscribeActiveEmbedEventsFromDatabase();
+    }
+
+    private void subscribeActiveEmbedEventsFromDatabase() {
+        EventDispatcher eventDispatcher = event.getClient().getEventDispatcher();
+        HashSet<EmbedEvent> activeEmbedEvents = Dependencies.getInstance()
+                .getEmbedEventService()
+                .getActiveEmbedEvents();
+
+        activeEmbedEvents.forEach(embedEvent -> {
+            EmbedBuilder embedBuilder = new EmbedBuilder(
+                    Dependencies.getInstance().getEmbedEventService(),
+                    Dependencies.getInstance().getSignupUserService()
+            );
+            embedBuilder.subscribeInteractions(eventDispatcher, embedEvent);
+        });
+
+        logger.log("Active EmbedEvent interactions subscribed!");
+    }
+}
