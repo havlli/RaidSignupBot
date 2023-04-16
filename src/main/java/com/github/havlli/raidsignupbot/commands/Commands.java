74a4fec2d03904cfe456aadee85cd724eaf30838
Havlli <havlliczech@gmail.com>
2023-04-16 18:04:37 +0200

    added commands to separated commands list and added json files for each command

---
+++ src/main/java/com/github/havlli/raidsignupbot/commands/Commands.java
@@ -0,0 +1,17 @@
+package com.github.havlli.raidsignupbot.commands;
+
+import java.util.List;
+
+public class Commands {
+
+    public static final List<String> COMMANDS = List.of(
+            "create-event.json",
+            "delete-event.json",
+            "test.json"
+    );
+
+    public static List<String> getCommandsList() {
+        return COMMANDS;
+    }
+
+}
