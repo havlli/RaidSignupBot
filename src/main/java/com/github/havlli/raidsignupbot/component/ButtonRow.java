967ea9f49b8cabeb50a84de95a593ca8810fd24f
Havlli <havlliczech@gmail.com>
2023-04-22 22:36:48 +0200

    added new component for handling ButtonRow creation -implemented builder

---
+++ src/main/java/com/github/havlli/raidsignupbot/component/ButtonRow.java
@@ -0,0 +1,65 @@
+package com.github.havlli.raidsignupbot.component;
+
+import discord4j.core.object.component.ActionRow;
+import discord4j.core.object.component.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ButtonRow implements ButtonRowComponent {
+
+    private final List<Button> buttons;
+    private final List<String> customIds;
+     public ButtonRow(Builder builder) {
+        this.buttons = builder.buttons;
+        this.customIds = builder.customIds;
+    }
+
+    @Override
+    public ActionRow getActionRow() {
+        return ActionRow.of(buttons);
+    }
+
+    @Override
+    public List<String> getCustomIds() {
+        return customIds;
+    }
+
+    public static ButtonRow.Builder builder() {
+        return new ButtonRow.Builder();
+    }
+
+    public static class Builder {
+        public enum buttonType {
+            PRIMARY,
+            SECONDARY
+        }
+        private final List<Button> buttons;
+        private final List<String> customIds;
+
+        Builder() {
+            this.buttons = new ArrayList<>();
+            this.customIds = new ArrayList<>();
+        }
+
+        public Builder addButton(String customId, String label, buttonType type) {
+
+            switch (type) {
+                case PRIMARY -> {
+                    buttons.add(Button.primary(customId, label));
+                    customIds.add(customId);
+                }
+                case SECONDARY -> {
+                    buttons.add(Button.secondary(customId, label));
+                    customIds.add(customId);
+                }
+            }
+
+            return this;
+        }
+
+        public ButtonRow build() {
+            return new ButtonRow(this);
+        }
+    }
+}
